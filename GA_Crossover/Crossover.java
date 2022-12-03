45994090a344d5f40bb2e57e6292e98e0291e6f1
asd <13904432217@163.com>
2022-12-03 02:37:07 +0000

    test functionality and add some comments to GA_Crossover

--- GA_Crossover/Crossover.java
+++ GA_Crossover/Crossover.java
@@ -2,6 +2,8 @@ package GA_Crossover;
 
 import GA_Runner.GeneticAlgorithm;
 
+//Crossover interface with GeneticAlgorithm instance to allow concrete 
+//classes to access GeneticAlgorithm object
 public interface Crossover {
     public GeneticAlgorithm demo = GeneticAlgorithm.getInstance();
     public void crossover();

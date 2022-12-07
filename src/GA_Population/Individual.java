70c62001f6e40e75baf5776ae113f0103b1a5245
Rayster Fernandes <rayster.fernandes@afourtech.com>
2022-12-07 22:58:55 +0000

    Added comments for Individual.java

--- src/GA_Population/Individual.java
+++ src/GA_Population/Individual.java
@@ -12,17 +12,15 @@ public class Individual<T extends Integer> {
 
     public int fitness = 0;
     public static int geneLength = 10; // Example gene : [0100011010]
-    public ArrayList<T> genes= new ArrayList<T>(); //ArrayList to store random generated genes
+    public ArrayList<T> genes= new ArrayList<T>(); //ArrayList to store random generated genes(0's and 1's)
 
 
 
 /**
- * This is the constructor of Individual where the genes are added and the fitness is intialized.
+ * This is the constructor of Individual where the genes are populated and the fitness is intialized.
  */
     public Individual() {
         Random rn = new Random();
-
-        
         for (int i = 0; i < geneLength; i++) {
             genes.add((T)new Integer(Math.abs(rn.nextInt() % 2)));
         }
